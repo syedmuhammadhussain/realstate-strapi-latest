2f941a18dad9559fc2129de168627ac66d5f3e89
syedmuhammadhussain <syedmuhammadhussain6@gmail.com>
2025-08-04 20:10:41 +0500

    update position logic

--- src/api/position/content-types/position/lifecycles.ts
+++ src/api/position/content-types/position/lifecycles.ts
@@ -1,63 +1,70 @@
-import { errors } from '@strapi/utils'
+import { errors } from "@strapi/utils";
 
 export default {
   async beforeCreate(event) {
     const {
       params: { data },
-    } = event
+    } = event;
 
     if (data.city && data.order !== undefined) {
       const duplicate = await strapi.db
-        .query('api::position.position')
+        .query("api::position.position")
         .findOne({
           where: {
             city: data.city.connect[0].id,
             order: data.order,
           },
-        })
+        });
 
       if (duplicate) {
         throw new errors.ValidationError(
           `The order value ${data.order} already exists for this city.`
-        )
+        );
       }
     }
   },
 
   async beforeUpdate(event) {
+    debugger;
     const {
       params: { data, where },
-    } = event
+    } = event;
 
     if ((data.order !== undefined || data.city) && data.order !== undefined) {
-      let city = data.city
-      let order = data.order
+      let city = data.city;
+      let order = data.order;
 
       if (!city || order === undefined) {
         const currentRecord = await strapi.db
-          .query('api::position.position')
+          .query("api::position.position")
           .findOne({
             where: { id: where.id },
-          })
-        if (!city) city = currentRecord.city
-        if (order === undefined) order = currentRecord.order
+          });
+        if (!city) city = currentRecord.city;
+        if (order === undefined) order = currentRecord.order;
+      }
+
+      if (city?.connect) {
+        city = city.connect[0].id;
+      } else if (typeof city === "object" && city.id) {
+        city = city.id;
       }
 
       const duplicate = await strapi.db
-        .query('api::position.position')
+        .query("api::position.position")
         .findOne({
           where: {
             city,
             order,
             id: { $ne: where.id },
           },
-        })
+        });
 
       if (duplicate) {
         throw new errors.ValidationError(
           `Position with order ${order} already exists for this city.`
-        )
+        );
       }
     }
   },
-}
+};
